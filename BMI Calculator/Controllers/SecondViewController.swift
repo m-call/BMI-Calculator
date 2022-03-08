1a2e02a5e8c650ef7e7d64e91b91bcb6a74129d4
Michael Callahan <mcallahan0@yahoo.com>
2022-03-07 20:31:55 -0500

    Created a new ViewController for our second view. Set up the basics of the new view controller.

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Michael Callahan on 3/7/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
