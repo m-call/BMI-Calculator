1e5b7b44df385ac86a3cb54483a15d28e2c3de45
Michael Callahan <mcallahan0@yahoo.com>
2022-03-09 08:00:49 -0500

    Changed bmi var to an optional and used the nil coalescing operator to unwrap the optional inside the getBMIValue method.

--- BMI Calculator/Models/CalculatorLogic.swift
+++ BMI Calculator/Models/CalculatorLogic.swift
@@ -2,21 +2,21 @@
 //  CalculatorLogic.swift
 //  BMI Calculator
 //
-//  Created by Colleen Callahan on 3/9/22.
+//  Created by Michael Callahan on 3/9/22.
 //
 
 import Foundation
 
 struct CalculatorLogic {
     
-    var bmi: Float = 0.0
+    var bmi: Float?
     
     mutating func calculateBMI(height: Float, weight: Float) {
         bmi = weight / pow(height, 2)
     }
     
     func getBMIValue() -> String {
-        let bmiValue = String(format: "%.1f", bmi)
+        let bmiValue = String(format: "%.1f", bmi ?? 0.0)
         return bmiValue
     }
     
